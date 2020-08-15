2345ab6145b3512191be7cd43747eb5379f728c1
Michael Kirsch <mkirsch.git@gmail.com>
2020-08-16 01:38:00 +0200

    add dependencies script

--- get_deps.sh
+++ get_deps.sh
@@ -1,3 +1,38 @@
 #!/bin/bash
 
+SCRIPTPATH=$(dirname "$(realpath -s "$0")")
 
+cd "$SCRIPTPATH"
+
+if [[ ! -d "$SCRIPTPATH/deps" ]]; then
+  mkdir deps
+  cd deps
+  git clone https://github.com/plerup/makeEspArduino.git
+  git clone https://github.com/esp8266/Arduino.git esp8266
+  git clone https://github.com/espressif/arduino-esp32.git esp32
+fi
+
+cd "$SCRIPTPATH/deps/makeEspArduino"
+git pull origin --quiet
+
+cd "$SCRIPTPATH/deps/esp8266"
+git fetch origin --tags --quiet
+GIT_TAG=$(git tag | grep -v 'rc' | tail -1)
+if [[ "$(git describe --tags)" != "$GIT_TAG" ]]; then
+  git checkout --quiet tags/$GIT_TAG
+  git submodule update --init --quiet
+  cd tools
+  python get.py
+fi
+
+cd "$SCRIPTPATH/deps/esp32"
+git fetch origin --tags --quiet
+GIT_TAG=$(git tag | grep -v 'rc' | tail -1)
+if [[ "$(git describe --tags)" != "$GIT_TAG" ]]; then
+  git checkout --quiet tags/$GIT_TAG
+  git submodule update --init --quiet
+  cd tools
+  python get.py
+fi
+
+cd "$SCRIPTPATH"
