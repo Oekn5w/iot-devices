abb63a421785257dc2707e6775562d3d25fa28d9
Michael Kirsch <mkirsch.git@gmail.com>
2020-08-24 20:48:58 +0200

    prepare single shutter multi environment

---
+++ src/shutter-1/make-all.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+SOURCEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $SOURCEDIR
+
+if [[ -n "$@" ]]; then
+
+EXIT=1
+
+for i in "$@"
+do
+
+case $i in
+  ota)
+  EXIT=0
+  ;;
+  clean)
+  EXIT=0
+  ;;
+  *)
+  ;;
+esac
+done
+
+if (( $EXIT )); then
+  echo "Only ota or build actions allowed for all."
+  exit 1
+fi
+
+fi
+
+make SHUTTER_ID=1 $@
+
+make SHUTTER_ID=2 $@
+
+make SHUTTER_ID=3 $@
