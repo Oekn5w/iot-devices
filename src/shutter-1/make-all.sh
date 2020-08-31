185f511c8639c2a3dd9f089115ae2872f79c7c14
Michael Kirsch <mkirsch.git@gmail.com>
2020-08-31 17:13:41 +0200

    fix intendation and use number of args

--- src/shutter-1/make-all.sh
+++ src/shutter-1/make-all.sh
@@ -4,29 +4,29 @@ SOURCEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $SOURCEDIR
 
-if [[ -n "$@" ]]; then
+if [[ $# -ne 0 ]]; then
 
-EXIT=1
+  EXIT=1
 
-for i in "$@"
-do
+  for i in "$@"
+  do
 
-case $i in
-  ota)
-  EXIT=0
-  ;;
-  clean)
-  EXIT=0
-  ;;
-  *)
-  ;;
-esac
-done
+  case $i in
+    ota)
+    EXIT=0
+    ;;
+    clean)
+    EXIT=0
+    ;;
+    *)
+    ;;
+  esac
+  done
 
-if (( $EXIT )); then
-  echo "Only ota or build actions allowed for all."
-  exit 1
-fi
+  if (( $EXIT )); then
+    echo "Only ota or build actions allowed for all."
+    exit 1
+  fi
 
 fi
 
