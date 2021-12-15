80c7e946110626346f144a9cf261b6dd8931e316
Michael Kirsch <mkirsch.git@gmail.com>
2021-12-15 20:13:11 +0100

    move to extra HW config

---
+++ src/shutter-3/hw_config.h
@@ -0,0 +1,20 @@
+#define BOARD_PIN_TEMP (36)
+
+#define PIN_A_OUT_1 (21)
+#define PIN_A_OUT_2 (32)
+#define PIN_A_IN_1 (17)
+#define PIN_A_IN_2 (16)
+#define PIN_A_TEMP (39)
+
+#define PIN_B_OUT_1 (26)
+#define PIN_B_OUT_2 (25)
+#define PIN_B_IN_1 (22)
+#define PIN_B_IN_2 (27)
+#define PIN_B_TEMP (35)
+
+#define PIN_C_OUT_1 (13)
+#define PIN_C_OUT_2 (23)
+#define PIN_C_IN_1 (18)
+#define PIN_C_IN_2 (19)
+#define PIN_C_TEMP (34)
+
