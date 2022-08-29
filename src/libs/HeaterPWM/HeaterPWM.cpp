994102e94e075ef877768e1ae99b30806363cdb3
Michael Kirsch <mkirsch.git@gmail.com>
2022-08-29 19:46:38 +0200

    enable capping PWM signals

--- src/libs/HeaterPWM/HeaterPWM.cpp
+++ src/libs/HeaterPWM/HeaterPWM.cpp
@@ -5,6 +5,10 @@
 #define PWM_FREQ (1000)
 #define PWM_RES_BITS (10)
 #define PWM_DC_MAX ((1<<(PWM_RES_BITS)) - 1)
+#define PWM_CAPPED (1)
+#define PWM_DC_CAP_LOW (2)
+#define PWM_DC_CAP_HIGH (PWM_DC_MAX - PWM_DC_CAP_LOW)
+static_assert(PWM_DC_CAP_HIGH > PWM_DC_CAP_LOW);
 
 #define MSG_BUFFER_SIZE	(10)
 
@@ -22,10 +26,18 @@ void HeaterPWM::setup()
 {
   ledcSetup(this->channel, PWM_FREQ, PWM_RES_BITS);
   ledcAttachPin(this->pin, this->channel);
-#if PWM_INVERSE > 0
-  ledcWrite(this->channel, PWM_DC_MAX);
+#if PWM_CAPPED > 0
+  #if PWM_INVERSE > 0
+  ledcWrite(this->channel, PWM_DC_MAX - PWM_DC_CAP_LOW);
+  #else
+  ledcWrite(this->channel, PWM_DC_CAP_LOW);
+  #endif
 #else
+  #if PWM_INVERSE > 0
+  ledcWrite(this->channel, PWM_DC_MAX);
+  #else
   ledcWrite(this->channel, 0);
+  #endif
 #endif
   this->timeout = 0;
 }
@@ -73,11 +85,22 @@ void HeaterPWM::processTarget(uint16_t dutyCycle)
   }
   if (this->state != dutyCycle)
   {
+  #if PWM_CAPPED > 0
+    uint16_t capped = dutyCycle;
+    if (capped > PWM_DC_CAP_HIGH) capped = PWM_DC_CAP_HIGH;
+    if (capped < PWM_DC_CAP_LOW) capped = PWM_DC_CAP_LOW;
+    #if PWM_INVERSE > 0
+    ledcWrite(this->channel, PWM_DC_MAX - capped);
+    #else
+    ledcWrite(this->channel, capped);
+    #endif
+  #else
     #if PWM_INVERSE > 0
     ledcWrite(this->channel, PWM_DC_MAX - dutyCycle);
     #else
     ledcWrite(this->channel, dutyCycle);
     #endif
+  #endif
     this->state = dutyCycle;
     this->publish();
   }
